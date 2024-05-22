a7ffba92104427fe4d149f145f1c1190a636ccf6
BlackNight414 <cristianariel375@gmail.com>
2024-05-21 21:03:30 -0300

    En clase Swap, el atributo operation_date tiene valor default la fecha actual, y en su test se evalúa con la fecha actual

--- app/models/swap.py
+++ app/models/swap.py
@@ -8,11 +8,11 @@ class Swap(db.Model):
     __tablename__ = 'swaps'
     
     id_swap: int = db.Column('id_swap', db.Integer, primary_key=True, autoincrement=True)
-    operation_date: datetime = db.Columnn('operation_date', db.DateTime, nullable=False)
+    operation_date: datetime = db.Columnn('operation_date', db.DateTime, nullable=False, default=datetime.utcnow()) # fecha actual por default
     amount_in: int = db.Column('amount_in', db.Integer, nullable=False)
     amount_out: int = db.Column('amount_out', db.Integer, nullable=False)
     id_wallet_in: int = db.Column('id_wallet_in', db.Integer, nullable=False)
-    id_wallet_out: int = db.Column('id_wallet_out', db.Intener, nullable=False)
+    id_wallet_out: int = db.Column('id_wallet_out', db.Integer, nullable=False)
 
     #wallet_in = db.relationship('Wallet', back_populates='')
     #wallet_out = db.relationship('Wallet', back_populates='')
