e8e1b98ecfc9b2e6605c73f3dbddc93e43fa8ce6
Pamela-guevara <pame18287@gmail.com>
2024-06-17 17:58:06 -0300

    Correcciones en test de coin

--- test/test_coin.py
+++ test/test_coin.py
@@ -1,7 +1,6 @@
 import unittest
 from flask import current_app
-from app import create_app
-
+from app import create_app, db
 from app.models import Coin
 
 
@@ -10,6 +9,7 @@ class CoinTestCase (unittest.TestCase):
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.create_all()
 
         #Datos de prueba / "clave" : valor,
         self.coin_data = {
@@ -20,14 +20,17 @@ class CoinTestCase (unittest.TestCase):
         }
 
     def tearDown(self):
-        self.app_context.pop() #Esto permite quita los datos de prueba con los que se ejecuto el test
+        #Esto permite quita los datos de prueba con los que se ejecuto el test
+        db.session.remove()
+        db.drop_all()
+
     
     def test_coin(self):
             coin = Coin(**self.coin_data) # ** significa que puede contener varios argumentos
 
-            self.assertTrue(coin.id_coin, coin.id_coin)
-            self.assertTrue(coin.coin_name, coin.coin_name)
-            self.assertTrue(coin.coin_abbreviation, coin.coin_abbreviation)
+            self.assertTrue(coin.id_coin, self.coin_data["id_coin"])
+            self.assertTrue(coin.coin_name, self.coin_data["coin_name"])
+            self.assertTrue(coin.coin_abbreviation, self.coin_data["coin_abbreviation"])
 
 if __name__ == "__main__":
      unittest.main() #Ejecuta todos los test que se encuentren en este archivo
