959f61ff37a45a6d9f83de187f747211a86852c9
Pamela-guevara <pame18287@gmail.com>
2024-06-17 21:42:37 -0300

    Borradas funciones dentro de modelos de wallet y coin. Descomentadas y modificadas relaciones entre wallet y coin, cambia a 1 - N

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -7,22 +7,6 @@ class Wallet(db.Model):
     balance =  db.Column('balance', db.Integer, nullable=False, default=0)
     id_owner_account = db.Column('id_account', db.ForeignKey('accounts.id_account'), nullable=False)
     accounts = db.relationship('Account', uselist=False, back_populates='wallets')
-    id_wallet_coin = db.Column('id_coin', db.ForeignKey('coins.id_coin'), nullable=False) #Buscar como agregarle en ondelete restrict -- no fuciona con ondelete='restrict'
-    #coin = db.relationship('Coin', uselist=False, back_populates='wallet')
-    def add_account(self, account):
-        if account not in self.accounts:
-            self.accounts.append(account)
-    
-    def remove_user(self, account):
-        if account in self.accounts:
-            self.accounts.remove(account)
-            
-    def add_coin(self, coin):
-        if coin not in self.coin:
-            self.coin.append(coin)
-    
-    def remove_user(self, coin):
-        if coin in self.coin:
-            self.coin.remove(coin)
-    
+    id_wallet_coin = db.Column('id_coin', db.ForeignKey('coins.id_coin'), nullable=False)
+    coin = db.relationship('Coin', back_populates='wallet')
     
