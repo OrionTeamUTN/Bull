504d818356168c02845fba020760ec09cc11fbb4
Willy <willygarcia75@gmail.com>
2024-08-30 20:16:13 -0300

    ruta del coin - save

---
+++ app/resources/coin.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request
+from app.schemas.coin_schema import coin_schema, coins_schema
+
+from app.services.coin_services import CoinServices
+
+# instanciar el objeto Blueprint
+
+coin = Blueprint('coin',__name__, url_prefix='api/coins')
+coin_services = CoinServices()
+
+
+"""si en el 'services dice que lo que espera como argumento es un diccionario ... xej ..  def(.... algo:dict) 
+entonces en el 'save ()   sin argumentos
+pero va adentro '"""
+
+
+# tantos archivos como los que se ven en Schemas
+
+# y en cada archivo una ruta para cada funcion .. que las encuentro en 
+
+@coin.route('/save',method=['POST'])
+def save():
+    pass
+    coin = coin_schema.load(request.json)
+
+""" ESTAMOS HACIENDO BASICAMENTE:
+    dato = account_services.save(acc)
+    dato2 = account_schema.dump(dato)
+    dato3 = {"account" : dato2 }
+    return dato3 """
+
+    return{"coin": coin_schema.dump(coin_services.save(coin))}
+
+@coin.route('/delete',method=['DELETE'])
+def delete():
+    coin = 
+
+
+
